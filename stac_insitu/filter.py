de263ec268dfbfcb05ab97deaa840ce174e723ab
Justus Magin <keewis@posteo.de>
2024-03-20 14:35:16 +0100

    move the filter code into a new module

---
+++ stac_insitu/filter.py
@@ -0,0 +1,29 @@
+import geopandas as gpd
+import movingpandas as mpd
+import pandas as pd
+import shapely
+from tlz.itertoolz import remove
+
+
+def extract_trajectory(item):
+    geometry = item.geometry
+    coordinates = shapely.points(geometry["coordinates"])
+    time = pd.to_datetime(item.properties["time"], format="ISO8601").astype(
+        "datetime64[s]"
+    )
+
+    # TODO: find the crs info from the item properties
+    gdf = gpd.GeoDataFrame({"time": time}, geometry=coordinates, crs=4326).set_index(
+        "time"
+    )
+    return mpd.Trajectory(gdf, traj_id=item.id)
+
+
+def filter_trajectories(items, geometry, timespan):
+    def predicate(item):
+        traj = extract_trajectory(item)
+
+        segment = traj.get_segment_between(*pd.to_datetime(timespan))
+        return not segment.intersects(geometry)
+
+    return list(remove(predicate, items))
